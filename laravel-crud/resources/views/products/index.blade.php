f1008e45327118089404a53dc9c5f2a682ee9c90
Lit-23 <rommelmarquez143@gmail.com>
2024-02-04 19:13:24 +0800

    code along again to improve my basic fundamentals in laravel

--- laravel-crud/resources/views/products/index.blade.php
+++ laravel-crud/resources/views/products/index.blade.php
@@ -7,11 +7,23 @@
                 <a href="{{ route('products.create') }}" class="btn-link">Add Product</a>
             </div>
             @if($message = Session::get('success'))
-                <div>
-                    <ul>
-                        <li>{{ $message }}</li>
-                    </ul>
-                </div>
+                <script type="text/javascript">
+                    const Toast = Swal.mixin({
+                        toast: true,
+                        position: "top-end",
+                        showConfirmButton: false,
+                        timer: 3000,
+                        timerProgressBar: true,
+                        didOpen: (toast) => {
+                            toast.onmouseenter = Swal.stopTimer;
+                            toast.onmouseleave = Swal.resumeTimer;
+                        }
+                        });
+                        Toast.fire({
+                        icon: "success",
+                        title: "{{ $message }}"
+                    });
+                </script>
             @endif
             <div class="table">
                 <div class="table-filter">
@@ -23,19 +35,21 @@
                         </ul>
                     </div>
                 </div>
-                <div class="table-search">   
-                    <div>
-                        <button class="search-select">
-                           Search Product
-                        </button>
-                        <span class="search-select-arrow">
-                            <i class="fas fa-caret-down"></i>
-                        </span>
+                <form method="GET" action="{{ route('products.index') }}" accept-charset="UTF-8" role="search">
+                    <div class="table-search">   
+                        <div>
+                            <button class="search-select">
+                                Search Product
+                            </button>
+                            <span class="search-select-arrow">
+                                <i class="fas fa-caret-down"></i>
+                            </span>
+                        </div>
+                        <div class="relative">
+                            <input class="search-input" type="text" name="search" placeholder="Search product..." value="{{ request('search') }}">
+                        </div>
                     </div>
-                    <div class="relative">
-                        <input class="search-input" type="text" name="search" placeholder="Search product..." value="{{ request('search') }}">
-                    </div>
-                </div>
+                </form>
                 <div class="table-product-head">
                     <p>Image</p>
                     <p>Name</p>
@@ -51,27 +65,45 @@
                             <p>{{ $product->category }}</p>
                             <p>{{ $product->quantity }}</p>
                             <div>     
-                                <button class="btn btn-success" >
+                                <a href="{{ route('products.edit', $product->id) }}" class="btn btn-success" >
                                     <i class="fas fa-pencil-alt" ></i> 
-                                </button>
-                                <button class="btn btn-danger" >
-                                    <i class="far fa-trash-alt"></i>
-                                </button>
+                                </a>
+                                <form action="{{route('products.destroy', $product->id)}}">
+                                    @method('delete')
+                                    @csrf
+                                    <button class="btn btn-danger" onclick="deleteConfirm(event)">
+                                        <i class="far fa-trash-alt"></i>
+                                    </button>
+                                </form>
                             </div>
                         @endforeach
                     @else
+                        <p>No Product Available Yet!</p>
                     @endif
                 </div>
                 <div class="table-paginate">
-                    <div class="pagination">
-                        <a href="#" disabled>&laquo;</a>
-                        <a class="active-page">1</a>
-                        <a>2</a>
-                        <a>3</a>
-                        <a href="#">&raquo;</a>
-                    </div>
+                    {{ $products->links('layouts.pagination') }}
                 </div>
             </div>
         </section>
     </main>
+    <script>
+        window.deleteConfirm = function (event){
+            event.preventDefault();
+            const form = event.target.form;
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+            if (result.isConfirmed) {
+                form.submit()
+            }
+            });
+        }
+    </script>
 @endsection
